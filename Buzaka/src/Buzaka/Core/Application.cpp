131c03b55c4d26271044afddf887fad57280a235
Ayyar Khalilov <super.ayar-04@yandex.ru>
2021-09-16 21:28:48 +0300

    Add missing OnAttach callbacks when pushing layers to the main applicaiton instance.

--- Buzaka/src/Buzaka/Core/Application.cpp
+++ Buzaka/src/Buzaka/Core/Application.cpp
@@ -45,9 +45,11 @@ namespace Buzaka {
 
     void Application::PushLayer(Layer *layer) {
         m_LayerStack.PushLayer(layer);
+        layer->OnAttach();
     }
 
     void Application::PushOverlay(Layer *overlay) {
         m_LayerStack.PushOverlay(overlay);
+        overlay->OnAttach();
     }
 }
