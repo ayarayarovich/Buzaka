f21d54d9526b0c4b1560a942e3daeeb22d28e34e
Ayyar Khalilov <super.ayar-04@yandex.ru>
2021-12-19 14:39:25 +0300

    Created custom keycodes

--- Buzaka/src/Buzaka.h
+++ Buzaka/src/Buzaka.h
@@ -3,9 +3,12 @@
 // For use by Buzaka applications
 #include "Buzaka/Core/EntryPoint.h"
 #include "Buzaka/Core/Application.h"
-#include "Buzaka/Core/Input.h"
 #include "Buzaka/Core/Log.h"
 #include "Buzaka/Core/Events.h"
 #include "Buzaka/Core/Layer.h"
 #include "Buzaka/Core/Window.h"
 #include "Buzaka/ImGui/ImGuiLayer.h"
+
+#include "Buzaka/Core/Input.h"
+#include "Buzaka/Core/KeyCodes.h"
+#include "Buzaka/Core/MouseButtonCodes.h"
